d36bfad34a2f92d3d543ab5c4bdcba14c3862d62
Tim Brier <t.brier93@gmail.com>
2022-02-04 14:49:12 +0000

    Add config for deployment from https://cloud.google.com/community/tutorials/kotlin-springboot-app-engine-java8

--- src/main/kotlin/com/timbrier/gcptictactoebackend/GcpTicTacToeBackendApplication.kt
+++ src/main/kotlin/com/timbrier/gcptictactoebackend/GcpTicTacToeBackendApplication.kt
@@ -2,9 +2,10 @@ package com.timbrier.gcptictactoebackend
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
-class GcpTicTacToeBackendApplication
+class GcpTicTacToeBackendApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
 	runApplication<GcpTicTacToeBackendApplication>(*args)
---
+++ src/main/kotlin/com/timbrier/gcptictactoebackend/api/GameController.kt
@@ -0,0 +1,15 @@
+package com.timbrier.gcptictactoebackend.api
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RestController
+
+data class Game(val id: String)
+
+@RestController
+class GameController {
+
+    @RequestMapping("/games")
+    fun getGames(): List<Game> {
+        return listOf(Game("game-id-0"), Game("game-id-1"))
+    }
+}
