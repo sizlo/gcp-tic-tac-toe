b17f2b77c4e456f2f9bc80e8d892d624021c7b59
Tim Brier <t.brier93@gmail.com>
2022-02-04 13:36:15 +0000

    Initial app generated from https://start.spring.io/

---
+++ src/main/kotlin/com/timbrier/gcptictactoebackend/GcpTicTacToeBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.timbrier.gcptictactoebackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GcpTicTacToeBackendApplication
+
+fun main(args: Array<String>) {
+	runApplication<GcpTicTacToeBackendApplication>(*args)
+}
