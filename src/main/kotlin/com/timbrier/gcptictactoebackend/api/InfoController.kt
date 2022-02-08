1c4001fb9f0ce8294c12ca4ef096795ccf06414b
Tim Brier <t.brier93@gmail.com>
2022-02-08 09:26:01 +0000

    Add endpoint for getting info about the running application

---
+++ src/main/kotlin/com/timbrier/gcptictactoebackend/api/InfoController.kt
@@ -0,0 +1,20 @@
+package com.timbrier.gcptictactoebackend.api
+
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class InfoController {
+
+    @GetMapping("/info")
+    fun getInfo(): Map<String, String> {
+        return mapOf(
+            envEntry("GAE_VERSION"),
+            envEntry("GAE_MEMORY_MB")
+        )
+    }
+
+    private fun envEntry(variableName: String): Pair<String, String> {
+        return variableName to System.getenv(variableName)
+    }
+}
